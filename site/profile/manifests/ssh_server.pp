9c1e171513229681bcd12b2a4cce7c30998afdcd
vvtregub <41521895+vvtregub@users.noreply.github.com>
2018-09-16 21:39:01 +0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -9,7 +9,7 @@ class profile::ssh_server {
     ssh_authorized_key {'root@master.puppet.vm':
         ensure => present,
         user   => 'root',
-        type   => 'ssh-rsa'
+        type   => 'ssh-rsa',
         key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDQlq0j6hAHaJhny8kuDppaEregfMp0ezxjsH+J09G4mvk4+MtgLzTLXnr6/sYcctRimcpyn0BWrKqkKXDM9nLOmLDgVP1HiE8v3wjqweqYwEcUqBYmIh8HS+cQaj7nAReB/zC9C17q8jNxn3bvYno7GGpoek1cORZhhsz18ybUQXwneyBi8YJmHGRVNS+2v8BD1G3AFUYrGZYqvwFivlCIyE1gtEO65MpdG+7Op5kbF07TAf2qRYoyqeeLVd6SFxBtnSx2u57XbZmOV9Jx6+byFeBQ7U1xRZ+IM9dv6CNzQ2N0/2Hw9C7JcjHlTItVsd3js0JOUpYSJkN2a1lbhLkB',
         }
 }        
